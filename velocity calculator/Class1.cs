482de978ba2f9d634f0a83b1ad90696fb5b6b730
Roberto <Roberto@DESKTOP-1818MJE>
2024-02-19 20:45:42 -0600

    Agregar archivos de proyecto.

---
+++ velocity calculator/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace velocity_calculator
+{
+    public class FinalVelocityCalculator
+    {
+        private double initialVelocity;        
+        private double acceleration;
+        private double time;
+
+        public FinalVelocityCalculator(double initialVelocity, double acceleration, double time)
+        {
+            this.initialVelocity = initialVelocity;
+            this.acceleration = acceleration;
+            this.time = time;
+        }
+
+        public double CalculateFinalVelocity()
+        {
+            return initialVelocity + (acceleration * time);
+        }
+    }
+}
---
+++ velocity calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using velocity_calculator;
+
+Console.WriteLine("Final Velocity Calculator");
+
+
+Console.Write("Enter the initial velocity (m/s): ");
+double initialVelocity = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the acceleration (m/s^2): ");
+double acceleration = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the time (s): ");
+double time = Convert.ToDouble(Console.ReadLine());
+
+FinalVelocityCalculator calculator = new FinalVelocityCalculator(initialVelocity, acceleration, time);
+double finalVelocity = calculator.CalculateFinalVelocity();
+
+Console.WriteLine($"The final velocity is: {finalVelocity} m/s");
+
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
